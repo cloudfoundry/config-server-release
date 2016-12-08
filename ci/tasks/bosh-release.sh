75a2904bdb739ba86f4d3de38cdb4c0c95c82ddc
Dale Wick <dwick@pivotal.io>
2016-12-08 14:45:21 -0500

    Add build release tasks for concourse
    [#135649083](https://www.pivotaltracker.com/story/show/135649083)
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

---
+++ ci/tasks/bosh-release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e -x
+
+install bosh-cli/bosh-cli* /usr/local/bin/bosh
+
+cd config-server-release
+bosh create-release --force --tarball=./config-server-release.tgz --name config-server --version acceptance
