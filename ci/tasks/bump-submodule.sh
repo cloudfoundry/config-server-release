7cbb070404f9ca36b5a701e59772c2e448fb1897
Saman Alvi <salvi@pivotal.io>
2017-01-04 18:00:54 -0500

    Updating release to build bosh release and bump sumbodule in separate jobs [#135982629] (https://www.pivotaltracker.com/story/show/135982629)

---
+++ ci/tasks/bump-submodule.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -x
+
+install bosh-cli/bosh-cli* /usr/local/bin/bosh
+
+cd config-server-release
+git checkout develop
+git branch -u origin/develop
+
+git submodule update --remote # pull in submodule changes, branch defined in .gitmodules
+
+git config --global user.email "submoduleupdate@localhost" 
+git config --global user.name "CI Submodule AutoUpdate"
+
+git add src/github.com/cloudfoundry/config-server
+
+if [[ $(git diff-index HEAD) ]]; then
+	git commit -m "Bumping config-server submodule"
+fi
+
+cd ..
+cp -R config-server-release output
