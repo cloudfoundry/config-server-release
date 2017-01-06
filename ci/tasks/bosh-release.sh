4f74e6d3c9cfb7535ee7e9734d36512321618b43
Saman Alvi <salvi@pivotal.io>
2017-01-06 14:19:54 -0500

    Setting submodule to point to develop
    [#135982629] (https://www.pivotaltracker.com/story/show/135982629)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/tasks/bosh-release.sh
+++ ci/tasks/bosh-release.sh
@@ -4,6 +4,9 @@ set -e -x
 install bosh-cli/bosh-cli* /usr/local/bin/bosh
 
 cd config-server-release
-git submodule update --remote # pull in submodule changes, branch defined in .gitmodules
+
+pushd src/github.com/cloudfoundry/config-server
+git pull origin develop
+popd
 
 bosh create-release --force --tarball=./config-server-release.tgz --name config-server --version acceptance
