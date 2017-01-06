0c4d1e420009c4cfca0557701f665a59b51f7c3a
Saman Alvi <salvi@pivotal.io>
2017-01-06 10:31:23 -0500

    CI Pipeline should auto bump config-server submodule to point to the latest SHA in the config-server-release repo
    [https://www.pivotaltracker.com/story/show/135982629] (#135982629)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/tasks/bosh-release.sh
+++ ci/tasks/bosh-release.sh
@@ -4,4 +4,6 @@ set -e -x
 install bosh-cli/bosh-cli* /usr/local/bin/bosh
 
 cd config-server-release
+git submodule update --remote # pull in submodule changes, branch defined in .gitmodules
+
 bosh create-release --force --tarball=./config-server-release.tgz --name config-server --version acceptance
