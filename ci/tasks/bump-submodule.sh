6f591781d1856f5213ebdf6f5c790cfdddb03126
Saman Alvi <salvi@pivotal.io>
2017-01-09 10:08:47 -0500

    Ensure that checkout of config-server submodule is the SHA being tested in update-submodule job
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/tasks/bump-submodule.sh
+++ ci/tasks/bump-submodule.sh
@@ -3,11 +3,16 @@ set -e -x
 
 install bosh-cli/bosh-cli* /usr/local/bin/bosh
 
-cd config-server-release
+cd config-server
+CONFIG_SERVER_SHA=$(git status | head -1 | cut -d " " -f4)
+
+cd ../config-server-release
 git checkout master
 git branch -u origin/master
 
-git submodule update --remote # pull in submodule changes, branch defined in .gitmodules
+pushd src/github.com/cloudfoundry/config-server
+git checkout $CONFIG_SERVER_SHA
+popd
 
 git config --global user.email "submoduleupdate@localhost" 
 git config --global user.name "CI Submodule AutoUpdate"
